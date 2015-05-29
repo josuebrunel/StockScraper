97eae23dc560192c01a1a1d980210b6fd70078b3
josuebrunel <josuebrunel@gmail.com>
2015-05-29 16:46:40 +0200

    test method for retrocompatibility added

--- tests.py
+++ tests.py
@@ -12,7 +12,12 @@ class TestStockRetriever(TestCase):
     def tearDown(self,):
         pass
 
-    def test_get_hisorical_info(self):
+    def test_get_hisorical_info_with_new_params(self):
 
         data = get_historical_info('YHOO',['Open','Close','High','Low'],startDate='2014-09-11',endDate='2015-02-10',limit=5)
+        print(data)
+
+    def test_get_hisorical_info_without_params(self):
+
+        data = get_historical_info('YHOO')
         print(data)
