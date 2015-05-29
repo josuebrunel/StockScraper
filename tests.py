d3ecda62da7f3c8edeef7b2063829dc75f6bf20c
josuebrunel <josuebrunel@gmail.com>
2015-05-29 16:50:44 +0200

    logging added and Travis CI

--- tests.py
+++ tests.py
@@ -1,4 +1,9 @@
+import logging
+
 from unittest import TestCase
+
+logging.basicConfig(level=logging.DEBUG,format="[%(asctime)s %(levelname)s] [%(name)s.%(module)s.%(funcName)s] %(message)s \n")
+logging.getLevelName('Test-StockScraper')
 
 from stockretriever import *
 
@@ -15,9 +20,9 @@ class TestStockRetriever(TestCase):
     def test_get_hisorical_info_with_new_params(self):
 
         data = get_historical_info('YHOO',['Open','Close','High','Low'],startDate='2014-09-11',endDate='2015-02-10',limit=5)
-        print(data)
+        logging.debug(data)
 
     def test_get_hisorical_info_without_params(self):
 
         data = get_historical_info('YHOO')
-        print(data)
+        logging.debug(data)
